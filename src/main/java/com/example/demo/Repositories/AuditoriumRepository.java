4b780d9caa7252a8e7cabdd3dc0115fd6af42d03
aiste334 <80888384+aiste334@users.noreply.github.com>
2021-10-13 10:31:11 +0200

    Repositories

---
+++ src/main/java/com/example/demo/Repositories/AuditoriumRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Entities.Auditorium;
+
+public interface AuditoriumRepository extends CrudRepository<Auditorium, Integer>{
+}
---
+++ src/main/java/com/example/demo/Repositories/ViewingRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Entities.Viewing;
+
+public interface ViewingRepository extends CrudRepository<Viewing, Integer>{
+}
