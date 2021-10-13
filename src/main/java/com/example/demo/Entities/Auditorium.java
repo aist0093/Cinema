96c948c186997a024a63039122e07b254ade2f35
annkarinacaro <anax1410@stud.kea.dk>
2021-10-13 10:56:52 +0200

    hhfh

--- src/main/java/com/example/demo/Entities/Auditorium.java
+++ src/main/java/com/example/demo/Entities/Auditorium.java
@@ -17,6 +17,40 @@ public class Auditorium {
     private int aisleNumber;
 
 
+    public int getAuditoriumId() {
+        return auditoriumId;
+    }
 
 
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    public int getAisleNumber() {
+        return aisleNumber;
+    }
+
+    public void setAisleNumber(int aisleNumber) {
+        this.aisleNumber = aisleNumber;
+    }
 }
