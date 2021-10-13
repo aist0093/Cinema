b60ae2a26fdd2c51f08cb665f2dbef66d8a54c10
annkarinacaro <anax1410@stud.kea.dk>
2021-10-13 10:56:52 +0200

    hhfh

--- src/main/java/com/example/demo/Entities/Auditorium.java
+++ src/main/java/com/example/demo/Entities/Auditorium.java
@@ -1,15 +1,19 @@
 package com.example.demo.Entities;
 
 
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 
 @Entity
+@NoArgsConstructor
 @Table(name="auditorium", schema="demo31")
 public class Auditorium {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "auditorium_id")
     private int auditoriumId;
     private int locationId;
     private String name;
@@ -17,6 +21,40 @@ public class Auditorium {
     private int aisleNumber;
 
 
+    public int getAuditoriumId() {
+        return auditoriumId;
+    }
 
 
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    public int getAisleNumber() {
+        return aisleNumber;
+    }
+
+    public void setAisleNumber(int aisleNumber) {
+        this.aisleNumber = aisleNumber;
+    }
 }
--- src/main/java/com/example/demo/Entities/Movie.java
+++ src/main/java/com/example/demo/Entities/Movie.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Movie {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int movieId;
     private String imdbId;
     private int ageRequirement;
