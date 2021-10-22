fc904608754f5a019bbde93e50802abbd445ec1b
justaszdanavicius <zdanaviciusjustas1@gmail.com>
2021-10-22 19:39:40 +0200

    env vars

--- src/main/java/com/example/demo/CinemaProjectApplication.java
+++ src/main/java/com/example/demo/CinemaProjectApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CinemaProjectApplication {
 
     public static void main(String[] args) {
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        System.out.println("---------${IP_ADDRESS}----------");
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
+        System.out.println("--------------------------------");
         SpringApplication.run(CinemaProjectApplication.class, args);
     }
 
